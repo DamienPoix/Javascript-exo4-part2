6dde62ba2f89f5ccdb7b28b8b6a0fb7703eaf4c5
DamienPoix <damienpoix@live.fr>
2018-06-22 15:05:56 +0200

    exercice 4 partie 2

---
+++ assets/js/fonction.js
@@ -0,0 +1,8 @@
+document.getElementById("reset").onclick = function() {getInfo()};
+//description function
+function getInfo(){
+  //récupération des informations pour les modifier
+document.getElementById('lastname').value ="";
+document.getElementById('firstname').value ="";
+document.getElementById('city').value ="";
+}
